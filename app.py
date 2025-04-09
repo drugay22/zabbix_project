5561eb983c6d0243571a12be084cf51d973fb07f
Your Name <you@example.com>
2025-04-09 16:01:21 +0300

    Updated app.py with new changes

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ def get_zabbix_data():
         "Content-Type": "application/json"
     }
     
-    # Используйте свой API токен
     auth_token = "d21afa4160a0826fe5966fdbe71001c19fa6e310fba7beeced2456aa8db2f783"  # Ваш токен из Zabbix
 
     # Запрос на получение хостов
@@ -33,12 +32,23 @@ def get_zabbix_data():
         "params": {
             "output": "extend"
         },
-        "auth": auth_token,  # Используем ваш токен
+        "auth": auth_token,
         "id": 2
     }
 
     response = requests.post(url, headers=headers, data=json.dumps(data))
-    hosts = response.json().get("result")
-    return hosts
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)  # Изменение порта на 5001
+    
+    # Печать статуса и текста ответа для отладки
+    print(f"Status Code: {response.status_code}")
+    print(f"Response Text: {response.text}")
+
+    try:
+        response.raise_for_status()  # Проверка на ошибки HTTP
+        hosts = response.json().get("result")
+        return hosts
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return []
+    except json.JSONDecodeError:
+        print("Error: Failed to decode JSON")
+        return []
